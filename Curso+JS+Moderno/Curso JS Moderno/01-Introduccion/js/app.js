6fdbe3e79be1bc6b2291eb466251c05e1ddd684f
HectorManu <le19081442@itmerida.edu.mx>
2022-08-30 13:40:49 -0500

    Eestoy agregando cambis a la carpeta de introducción en la cual vemos la introducción a JavaScript

---
+++ Curso+JS+Moderno/Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,17 @@
+/*
+//Con esta parte podemos manejar el código de html 
+
+// Pregunta al usuario su nombre 
+const nombre = prompt('¿cuál es tu nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = ` ${nombre} está aprendiendo JavaScript Moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
+*/
+
+// CREANDO OBJETOS
+
+console.log('holamundo')
+console.log('hola')
