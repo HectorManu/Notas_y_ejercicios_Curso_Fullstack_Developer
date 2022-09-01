6337e9a685bf99b2b0fb3f8a6c95f9b088aefd83
HectorManu <le19081442@itmerida.edu.mx>
2022-09-01 16:18:37 -0500

    Ya estoy en la carpeta de números y pues estoy avanzando jeje

--- Curso+JS+Moderno/Curso JS Moderno/03-Strings/js/07-app.js
+++ Curso+JS+Moderno/Curso JS Moderno/03-Strings/js/07-app.js
@@ -1,1 +1,13 @@
 const producto = 'Monitor 20 Pulgadas';
+console.log(producto.toUpperCase());// ESto es para volver todo a mayusculas
+
+console.log(producto.toLowerCase()); //lo pasamos a minusculas
+
+const email = "CORREO@CORREO.COM"
+
+console.log(email.toLowerCase()); //lo pasamos a minusculas
+
+const precio = 300;
+
+// Hacesuna conversión de de entero a String
+console.log(precio.toString());
---
+++ Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,10 @@
+/*// Creación de números
+
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(numero1)
+console.log(numero2)//este en la consula aparece con otro formato  */
+
+const numero1 =30;
+// puedes poner números de manera que no declares el tipo de número que será.
