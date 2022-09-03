72493d8957c89dbf1478fbbeb1d0fc4787ad1af4
HectorManu <le19081442@itmerida.edu.mx>
2022-09-02 21:37:38 -0500

    Agregué nejercicios con números en JavaScript

--- Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/01-app.js
+++ Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/01-app.js
@@ -8,3 +8,12 @@ console.log(numero2)//este en la consula aparece con otro formato  */
 
 const numero1 =30;
 // puedes poner números de manera que no declares el tipo de número que será.
+
+// TODOS LOS NÚMEROS SE CREAN 
+
+//forma no muy común de crar números
+
+const numero6 = new Number(80);
+console.log(numero6)
+
+// Los números pueden ser negativos y positivos con javascript
---
+++ Curso+JS+Moderno/Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,31 @@
+// Ejercicios
+
+const numero1 = 30;
+const numero2 = 20;
+
+let resultado;
+
+// suma
+resultado = numero1+numero2;
+
+// Resta 
+
+resultado = numero1-numero2
+
+// multiplicación
+
+resultado = numero1* numero2;
+
+
+// división
+
+resultado= numero1/numero2;
+
+// modulo
+
+resultado = numero1%numero2;//este es el residuo que queda de la división
+
+
+
+
+console.log(resultado);
