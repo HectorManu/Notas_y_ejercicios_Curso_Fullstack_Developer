43522f28d7c5292cf3ccebcee79cbcd2f24bc3e1
HectorManu <le19081442@itmerida.edu.mx>
2022-09-06 13:51:12 -0500

    hablando algo

---
+++ Curso+JS+Moderno/Curso JS Moderno/06-Booleans/js/01-app.js
@@ -0,0 +1,13 @@
+// un bolleano es un valor verdadero o falso
+const bolean1 = true;
+const bolean2 = false;
+const boolean3 = "true";
+
+// no pasa con los bolleans l hecho de un comparador estricto el que sean iguales como en los números
+
+// console.log(bolean1);
+// console.log(bolean2);
+
+
+const boolean4 = new Boolean(true);
+console.log(typeof(boolean4));
