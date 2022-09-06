3fa861fe2b39f8f783035b92e504eb23a19a3837
HectorManu <le19081442@itmerida.edu.mx>
2022-09-06 14:12:00 -0500

    subí cosas

--- Curso+JS+Moderno/Curso JS Moderno/06-Booleans/js/01-app.js
+++ Curso+JS+Moderno/Curso JS Moderno/06-Booleans/js/01-app.js
@@ -6,8 +6,9 @@ const boolean3 = "true";
 // no pasa con los bolleans l hecho de un comparador estricto el que sean iguales como en los números
 
 // console.log(bolean1);
-// console.log(bolean2);
+// console.log(bolean2); aquyí agrego algo
 
 
 const boolean4 = new Boolean(true);
 console.log(typeof(boolean4));
+
