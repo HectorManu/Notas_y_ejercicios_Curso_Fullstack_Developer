2e50d549f6340a82e1e7a1649a20d99240dc1ffe
HectorManu <le19081442@itmerida.edu.mx>
2022-09-06 15:53:38 -0500

    Agregué notas a la carpeta de Boleans en la cual barcamos las partes importantes de su uso

---
+++ Curso+JS+Moderno/Curso JS Moderno/06-Booleans/js/02-app.js
@@ -0,0 +1,7 @@
+const bolean1 = true;
+const bolean2 = false;
+
+console.log(bolean1===bolean2);
+
+console.log(bolean1 ===true);
+console.log(bolean1 === "true");//no funciona como los números 
---
+++ Curso+JS+Moderno/Curso JS Moderno/06-Booleans/js/03-app.js
@@ -0,0 +1,17 @@
+// consejos para utilizar booleand 
+
+
+ const autenticado = true;
+
+
+
+// if(autenticado===true){ LA MANERA CORRECTA ES  IF (AUTENTICADO){} Y TODA LA ESTRUCTURA DE ABAJO 
+//     console.log('Sí puedes ver netflix')
+// }else {
+//     console.lot('No no puedes verlo')
+// } 
+
+
+// OPERADOR TERNARIO 
+
+console.log(autenticado ? 'Si esta autenticado ' : 'No estas autenticado');
