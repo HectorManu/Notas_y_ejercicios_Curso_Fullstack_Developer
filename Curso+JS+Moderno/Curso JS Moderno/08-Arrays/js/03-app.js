e277707730ce3bb1d495bf1f79874178acc3bdbe
HectorManu <le19081442@itmerida.edu.mx>
2022-10-05 22:05:35 -0500

    termine el ejercicio 3

---
+++ Curso+JS+Moderno/Curso JS Moderno/08-Arrays/js/03-app.js
@@ -0,0 +1,14 @@
+const meses = ['enero','febrero','marzo']
+
+console.table(meses);
+
+// recorrer un arreglo 
+// para ello necesitamos el tamao del arreblo
+
+console.log(meses.length);// método para ver el tamaño del arreglo
+
+for (let index = 0; index < meses.length; index++) {
+    console.log(meses[index]);
+    const element = meses[index];
+    
+}
