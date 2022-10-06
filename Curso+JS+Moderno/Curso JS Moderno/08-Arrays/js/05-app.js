e66272bd4e816ee5ba384028fa48e4523c68c0f9
HectorManu <le19081442@itmerida.edu.mx>
2022-10-05 22:14:32 -0500

    termine el ejercicio 5

---
+++ Curso+JS+Moderno/Curso JS Moderno/08-Arrays/js/05-app.js
@@ -0,0 +1,30 @@
+// const meses = ['enero','febrero','marzo'];
+
+// //agregar un nuevo mes con método pero sin conocer la extención del arreblo
+
+// meses.push('abril');
+// meses.push('mayo');
+
+// console.table(meses);
+
+
+// ejemplo de carrito 
+const carrito = [];
+
+// definir un procucto 
+const producto = {
+    nombre : 'monitor de 24 pulgadas',
+    precio : 2400
+}
+
+const producto2 = {
+    nombre : 'Iphone',
+    precio : 25000
+}
+const producto3={
+    nombre : 'mousepad',
+    precio : 49
+}
+carrito.push(producto,producto2);//coloca al final
+carrito.unshift(producto3);//para agregarlo hasta el inicio 
+console.table(carrito)
