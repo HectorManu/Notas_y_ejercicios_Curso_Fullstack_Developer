ebeeb786e286b8f619e79ddf1e09b5fad8395b98
HectorManu <le19081442@itmerida.edu.mx>
2022-10-06 15:32:27 -0500

    vimos las manera de declarar una función

---
+++ Curso+JS+Moderno/Curso JS Moderno/09-Funciones/js/01-app.js
@@ -0,0 +1,22 @@
+// las funciones son reutilizables
+
+// DOS MANERA DE CREAR UNA FUNCIÓN
+// declración de ffunción 
+// expreción de función 
+
+// DECLARACIÓN DE FUNCIÓN (FUNTION DECLRATION)
+
+function sumar() {
+    console.log(2+2);
+}
+
+// ALGO IMPORTANTE ES MANDAR A LLAMAR LA FUNCIÓN
+
+sumar();
+
+// expresión de función (funtionexpression)
+
+const sumar2 =function () {
+    console.log(3+6);
+}
+sumar2();
