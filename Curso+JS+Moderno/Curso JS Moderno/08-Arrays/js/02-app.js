753b5bcf3280d7e0ce151f5d7c9f573dd84596b4
HectorManu <le19081442@itmerida.edu.mx>
2022-10-05 21:58:49 -0500

    termine el ejercicio 2

---
+++ Curso+JS+Moderno/Curso JS Moderno/08-Arrays/js/02-app.js
@@ -0,0 +1,14 @@
+const numeros = [1,2,3];
+const numeros2 = [1,2,3,[1,2]];
+
+console.log(numeros);
+// manera de viaulizar mejorada
+console.table(numeros);
+
+
+// consultar un valor especifico 
+
+console.log(numeros[2]); // recordar que el indice inicia desde cero
+
+console.log(numeros2[3][0]);
+
