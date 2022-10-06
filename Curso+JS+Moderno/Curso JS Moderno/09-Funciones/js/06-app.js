f6c9f338f438bafe0901e9559df6ab20baf28ea2
HectorManu <le19081442@itmerida.edu.mx>
2022-10-06 15:50:26 -0500

    termine el ejercicio 6

---
+++ Curso+JS+Moderno/Curso JS Moderno/09-Funciones/js/06-app.js
@@ -0,0 +1,11 @@
+
+
+function saludar(nombre='desconocido',apellido='no puso apellido') {
+    console.log(`Hola ${nombre} ${apellido}`);    
+}
+
+saludar('hector');
+saludar('','ruiz');
+saludar('hector','ruiz');
+
+saludar();
