1696744d64eda19632584f4d673c0b1c3e4c4412
HectorManu <le19081442@itmerida.edu.mx>
2022-10-13 11:40:24 -0500

    termine el ejercicio 3

--- Curso+JS+Moderno/Curso JS Moderno/10-Estructuras/js/03-app.js
+++ Curso+JS+Moderno/Curso JS Moderno/10-Estructuras/js/03-app.js
@@ -1,6 +1,6 @@
 const dinero = 500;
 const totalApagar = 300;
 
-if (dinero > totalApagar) {
+if (dinero >= totalApagar) {
     console.log(`SI PODEMOS PAGAR`);
 }else console.log(`fonsos insuficientes`);
