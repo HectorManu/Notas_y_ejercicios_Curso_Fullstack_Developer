91afbda9c14a6b533ab5d8bc0122e6179b856700
HectorManu <le19081442@itmerida.edu.mx>
2022-10-13 11:48:12 -0500

    termine el ejercicio 5

---
+++ Curso+JS+Moderno/Curso JS Moderno/10-Estructuras/js/05-app.js
@@ -0,0 +1,14 @@
+//switch case
+
+const metodoPago = 'efectivo';
+
+
+switch (metododepago) {
+    case 'efectivo':
+        console.log('PAGASTE CON EFECTIVO')
+        break;
+
+    default:
+        console.log('aún no has sleccionado un método de pago o método de pago no soportado')
+        break;
+}
