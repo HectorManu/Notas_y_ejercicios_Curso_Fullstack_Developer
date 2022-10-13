022a46482ddaf43674bb33feb57fce716a68c58d
HectorManu <le19081442@itmerida.edu.mx>
2022-10-13 11:30:38 -0500

    termine el primer eercicio de estrucutras

---
+++ Curso+JS+Moderno/Curso JS Moderno/10-Estructuras/js/01-app.js
@@ -0,0 +1,12 @@
+// cuando deseamos que nuestro códio se ejecute en diferente orden
+
+// las estructuras de control son para ver si se cumple una determinada condicion que peus nos ayudara
+
+const puntaje = 100;
+
+if (puntaje==100) {//si esta condicion se cumple se eecuta lo que hay en estar llaves 
+    console.log(`si es igual`)
+}else console.log(`NO SE igual`);
+
+if (puntaje==100)console.log(`si es igual`); else console.log(`NO SE igual`);
+
