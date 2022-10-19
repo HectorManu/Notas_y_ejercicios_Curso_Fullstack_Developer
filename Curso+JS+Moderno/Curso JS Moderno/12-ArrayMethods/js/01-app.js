c7d65457f870fb8d77a3bcbedf3926c88478733c
HectorManu <le19081442@itmerida.edu.mx>
2022-10-19 12:00:10 -0500

    cambios en las tablas

--- Curso+JS+Moderno/Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Curso+JS+Moderno/Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -1,7 +1,7 @@
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
 const carrito = [
-    { nombre: 'Monitor 27 Pulgadas', precio: 500 },
+    { nombre: 'Monitor 34 Pulgadas', precio: 500 },
     { nombre: 'Televisión', precio: 100 },
     { nombre: 'Tablet', precio: 200 },
     { nombre: 'Audifonos', precio: 300 },
